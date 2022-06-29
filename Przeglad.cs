18be724116e0a4c974e6b3f725ffb65c33c6100d
Krzysiek354 <81149559+Krzysiek354@users.noreply.github.com>
2022-06-29 09:01:07 +0200

    Add files via upload

---
+++ Przeglad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace projekt_p4_konsola
+{
+    public partial class Przeglad
+    {
+        public int Idprzegladu { get; set; }
+        public DateTime DataPrzegladu { get; set; }
+        public string ZakresPrzegladu { get; set; } = null!;
+        public string WykonujacyPrzegladImie { get; set; } = null!;
+        public string WykonujacyPrzegladNazwisko { get; set; } = null!;
+        public string? Zalecenia { get; set; }
+        public int? Idmostu { get; set; }
+
+        public virtual Most? IdmostuNavigation { get; set; }
+    }
+}
