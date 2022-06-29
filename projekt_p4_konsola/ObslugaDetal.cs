80797f39767d0ebff5b3ede9ba4956d25187853b
Krzysiek354 <81149559+Krzysiek354@users.noreply.github.com>
2022-06-29 09:14:39 +0200

    Add files via upload

---
+++ projekt_p4_konsola/ObslugaDetal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace projekt_p4_konsola
+{
+    public partial class ObslugaDetal
+    {
+        public int? Idmostu { get; set; }
+        public int? NumerKwalifikacji { get; set; }
+
+        public virtual Most? IdmostuNavigation { get; set; }
+        public virtual OsobaObslugujaca? NumerKwalifikacjiNavigation { get; set; }
+    }
+}
---
+++ projekt_p4_konsola/OsobaObslugujaca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace projekt_p4_konsola
+{
+    public partial class OsobaObslugujaca
+    {
+        public int NumerKwalifikacji { get; set; }
+        public string Imie { get; set; } = null!;
+        public string Nazwisko { get; set; } = null!;
+        public string? RodzajKwalifikacji { get; set; }
+    }
+}
